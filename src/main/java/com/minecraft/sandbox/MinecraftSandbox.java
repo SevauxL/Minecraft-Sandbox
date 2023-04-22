7d28e748c53c4839d4e577c713f414b9a1ad58b1
Leeroy Sevaux <sevaux.leeroy@gmail.com>
2023-04-22 16:24:16 +0200

    Testing command creation

--- src/main/java/com/minecraft/sandbox/MinecraftSandbox.java
+++ src/main/java/com/minecraft/sandbox/MinecraftSandbox.java
@@ -1,11 +1,22 @@
 package com.minecraft.sandbox;
 
+import com.minecraft.sandbox.commands.HelloCommand;
 import com.minecraft.sandbox.listeners.BlockBreakListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MinecraftSandbox extends JavaPlugin {
     @Override
     public void onEnable() {
+        enableCommands();
         getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
+    }
+
+    @Override
+    public void onDisable() {}
+
+    // -----------------------
+
+    private void enableCommands() {
+        this.getCommand("hello").setExecutor(new HelloCommand());
     }
 }
---
+++ src/main/java/com/minecraft/sandbox/commands/HelloCommand.java
@@ -0,0 +1,18 @@
+package com.minecraft.sandbox.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class HelloCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if (commandSender instanceof Player) {
+            commandSender.sendMessage("Hello " + ChatColor.GOLD + commandSender.getName());
+        }
+        return false;
+    }
+}
